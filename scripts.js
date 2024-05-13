19789f41b77fa8fb6a88083acbfe38b075e85d4d
Lê Khôi <lndksky@gmail.com>
2024-05-13 15:37:53 +0700

    update intro

--- scripts.js
+++ scripts.js
@@ -1,3 +1,37 @@
+window.onload = function() {
+  // Scroll lên đầu trang
+  window.scrollTo(0, 0);
+
+  // Chặn scroll trong 5 giây
+  document.body.classList.add('no-scroll');
+  setTimeout(function() {
+      document.body.classList.remove('no-scroll');
+  }, 5000);
+
+  var intro = document.getElementById('intro');
+
+  // Hiển thị modal
+  intro.classList.add('show');
+
+  // Thay đổi màu nền sau 3 giây
+  setTimeout(function() {
+      intro.style.transition = 'background-color 1s';
+      intro.style.backgroundColor = '#E5D283';
+  }, 3000);
+
+// Ẩn modal dần dần sau 5 giây
+setTimeout(function() {
+  intro.style.transition = 'opacity 1s';
+  intro.style.opacity = '0';
+
+  // Biến mất hoàn toàn sau khi ẩn dần
+  setTimeout(function() {
+      intro.style.display = 'none';
+  }, 1000); // Thời gian này phải tương ứng với thời gian transition
+}, 5000);
+}
+
+
 window.onscroll = function() {scrollFunction()};
 
 var aboutReached = false; // Biến để kiểm tra xem đã đạt được phần "About" hay chưa
